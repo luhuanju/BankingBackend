7aa0603f07289c2f7acf52f65fc8e02ede609492
HUANJU.LU <huanjulu@huanjus-MBP.localdomain>
2023-03-24 08:35:43 -0700

    Initial commit

---
+++ src/main/java/com/cogent/bankingsys/BankingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.cogent.bankingsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingBackendApplication.class, args);
+	}
+
+}
