11abbb8eba5dd2b77197a1824a293885be93da63
Yuhao Fan <yuhao.fan@nyu.edu>
2023-05-07 16:34:57 -0500

    crossorigin

--- src/main/java/com/cogent/bankingsys/controller/BeneficiaryController.java
+++ src/main/java/com/cogent/bankingsys/controller/BeneficiaryController.java
@@ -8,18 +8,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.cogent.bankingsys.repo.BeneficiaryRepository;
 import com.cogent.bankingsys.entity.Beneficiary;
 
 @RestController
 @RequestMapping("/api/staff/beneficiary")
+@CrossOrigin(origins = "*")
 public class BeneficiaryController {
     @Autowired
     private BeneficiaryRepository beneficiaryRepository;
